24f08fd8f825951b64b1a542c4b8033a2b54c874
shamisashams <shamisaashams@gmail.com>
2022-03-23 13:30:14 +0400

    free text in spc box

--- resources/js/components/SpcBox/SpcBox.js
+++ resources/js/components/SpcBox/SpcBox.js
@@ -3,31 +3,32 @@ import { MainButton } from "../MainButton/MainButton";
 import "./SpcBox.css";
 
 const SpcBox = (props) => {
-  const [hiddenText, setHiddenText] = useState(false);
-  return (
-    <div className="spc_box" data-aos="zoom-out">
-      <div className="bold title">{props.offerTitle}</div>
-      <div className="for blue">{props.for}</div>
-      <div className="package">{props.package}</div>
-      <div className="price">
-        <span>₾</span>
-        {props.price}
-        <span>.00</span>
-      </div>
-      <div className="op05">{props.period}</div>
-      <p>{props.p1}</p>
-      <p>
-        {props.p2}: <span>{__("free")}</span> <br /> {props.p3}: <span>{__("free")}</span>
-      </p>
-      <p className={hiddenText ? "hidden_text display" : "hidden_text"}>
-        {props.description}
-      </p>
-      <MainButton
-        text={hiddenText ? __("see_less") : __("read_more")}
-        onClick={() => setHiddenText(!hiddenText)}
-      />
-    </div>
-  );
+    const [hiddenText, setHiddenText] = useState(false);
+    return (
+        <div className="spc_box" data-aos="zoom-out">
+            <div className="bold title">{props.offerTitle}</div>
+            <div className="for blue">{props.for}</div>
+            <div className="package">{props.package}</div>
+            <div className="price">
+                <span>₾</span>
+                {props.price}
+                <span>.00</span>
+            </div>
+            <div className="op05">{props.period}</div>
+            <p>{props.p1}</p>
+            <p>
+                {props.p2}: <span> {props.p2_deal}</span> <br /> {props.p3}:
+                <span>{props.p3_deal}</span>
+            </p>
+            <p className={hiddenText ? "hidden_text display" : "hidden_text"}>
+                {props.description}
+            </p>
+            <MainButton
+                text={hiddenText ? __("see_less") : __("read_more")}
+                onClick={() => setHiddenText(!hiddenText)}
+            />
+        </div>
+    );
 };
 
 export default SpcBox;
